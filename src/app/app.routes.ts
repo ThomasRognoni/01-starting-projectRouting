f4fdde46e184d88c17b1c204cb066a52c3356498
ThomasRognoni <thomas.rognoni@beyondoc.com>
2025-10-07 12:55:51 +0200

    splitting routes into a separate file and adding a not-found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './tasks/tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -12,15 +12,10 @@ export const routes: Routes = [
   {
     path: 'users/:userId', //<your-domain>/users/<uid>
     component: UserTasksComponent,
-    children: [
-        {
-            path: 'tasks', //<your-domain>/users/<uid>/tasks
-            component: TasksComponent,
-        },
-        {
-            path: 'tasks/new', //<your-domain>/users/<uid>/tasks/new
-            component: NewTaskComponent,
-        }
-    ]
+    children: userRoutes,
+  },
+  {
+    path: '**', //wildcard route for 404 not found pages
+    component: NotFoundComponent,
   }
 ];
